a9b5299f956c72a383b03abbbdfafcc604e1b0e9
Jihoon Seo <jihoon416@snu.ac.kr>
2022-12-05 08:53:08 +0900

    feat: CORS 활성화

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { PrismaService } from './prisma/prisma.service'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
+    // FIXME: 추후 localhost와 실제 배포된 웹에서만 동작하도록 수정 필요
+    app.enableCors()
+
     const prismaService = app.get(PrismaService)
     prismaService.enableShutdownHooks(app)
 
