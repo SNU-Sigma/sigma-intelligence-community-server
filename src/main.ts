5284335e9a02cde18341b70f2717cc33584c45d3
Jihoon Seo <jihoon416@snu.ac.kr>
2022-12-05 23:52:49 +0900

    feat: class-validator로 dto validation 추가

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { PrismaService } from './prisma/prisma.service'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
     // FIXME: 추후 localhost와 실제 배포된 웹에서만 동작하도록 수정 필요
     app.enableCors()
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
     const prismaService = app.get(PrismaService)
     prismaService.enableShutdownHooks(app)
