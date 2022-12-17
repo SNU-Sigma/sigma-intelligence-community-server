43a5bf7c2a2900588a25a684849cad8c6ae3c3cc
Jihoon Seo <jihoon416@snu.ac.kr>
2022-12-17 16:20:45 +0900

    feat: AWSService 생성 및 pre-signed url 생성 로직 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
 import { UsersModule } from './users/users.module'
 import { ConfigModule } from '@nestjs/config'
+import { AwsModule } from './aws/aws.module'
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         PrismaModule,
+        AwsModule,
         UsersModule,
     ],
     controllers: [AppController],
