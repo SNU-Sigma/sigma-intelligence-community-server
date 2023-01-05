7a737783100bcd8b465bb064cf30cf2a4e54a984
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-06 00:31:51 +0900

    feat: Implement JWT creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common'
 import { UsersService } from '../users/users.service'
+import { JwtService } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+        private configService: ConfigService,
+    ) {}
+
     async validateUser(email: string, password: string): Promise<User> {
         return { id: 0, email, password }
+    }
+
+    async login(user: User) {
+        return { accessToken: this.jwtService.sign({ email: user.email }) }
     }
 }
 
