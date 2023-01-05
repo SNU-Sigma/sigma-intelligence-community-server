c1eec74703a45bdaeed320842359138effa3d265
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-06 00:31:51 +0900

    feat: Implement JWT creation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,22 @@ import { AuthController } from './auth.controller'
 import { UsersModule } from 'src/users/users.module'
 import { PassportModule } from '@nestjs/passport'
 import { LocalStrategy } from './strategies/local.strategy'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-    imports: [UsersModule, PassportModule],
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET'),
+                expiresIn: '7d',
+            }),
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [AuthController],
     providers: [AuthService, LocalStrategy],
 })
