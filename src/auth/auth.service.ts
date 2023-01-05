ea4fcf29672b6b77e262349d1ae38457a48d882c
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-06 02:15:22 +0900

    feat: Connect database with JWT auth for login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,35 +1,54 @@
 import { Injectable } from '@nestjs/common'
-import { UsersService } from '../users/users.service'
 import { JwtService } from '@nestjs/jwt'
-import { ConfigService } from '@nestjs/config'
 import { JWTPayload } from './models/JWTPayload'
+import { PrismaService } from 'nestjs-prisma'
+import { User } from '@prisma/client'
+import { LoginCredentialsDto } from './dto/login-credentials.dto'
+import bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
     constructor(
-        private usersService: UsersService,
         private jwtService: JwtService,
-        private configService: ConfigService,
+        private prisma: PrismaService,
     ) {}
 
-    async validateUser(email: string, password: string): Promise<User> {
-        return { id: 0, email, password }
+    async validateUser({
+        email,
+        password,
+    }: LoginCredentialsDto): Promise<User | undefined> {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email,
+            },
+        })
+        if (user === null) {
+            return
+        }
+
+        const match = await bcrypt.compare(password, user.hashedPassword)
+
+        if (match === false) {
+            return
+        }
+        return user
     }
 
     async login(user: User) {
         const payload: JWTPayload = {
             id: user.id,
             email: user.email,
+            name: user.name,
         }
 
         return {
             accessToken: this.jwtService.sign(payload),
         }
     }
-}
 
-type User = {
-    id: number
-    email: string
-    password: string
+    async findUser(id: number) {
+        return this.prisma.user.findUnique({
+            where: { id },
+        })
+    }
 }
