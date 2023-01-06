58b9d0081b858792d3b9b034d4e61396eed12c20
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-06 23:40:03 +0900

    refactor: 불필요한 오브젝트 제거
    stateless한 JWT 사용으로 대신

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { LoginCredentialsDto } from './dto/login-credentials.dto'
 import bcrypt from 'bcrypt'
 import { ConfigService } from '@nestjs/config'
 import { AuthConstants } from './auth.constants'
+import { MagicLinkPayload } from './models/MagicLinkPayload'
 
 @Injectable()
 export class AuthService {
@@ -16,52 +17,35 @@ export class AuthService {
         private configService: ConfigService,
     ) {}
 
-    private temporaryToken: { [key in string]: string } = {}
-
-    private cleanUpExpiredTokens() {
-        Object.entries(this.temporaryToken).forEach(([email, token]) => {
-            try {
-                this.jwtService.verify(token)
-            } catch (e) {
-                delete this.temporaryToken[email]
-            }
-        })
-    }
-
     async createMagicLink(email: string): Promise<string> {
-        this.cleanUpExpiredTokens()
-        const token = await this.jwtService.sign(
-            { email },
-            {
-                expiresIn: '30m',
-            },
-        )
-        this.temporaryToken[email] = token
+        const payload: MagicLinkPayload = { email }
+        const token = await this.jwtService.signAsync(payload, {
+            expiresIn: '30m',
+        })
         return `${AuthConstants.magicLinkBaseUrl}/set-password?token=${token}&email=${email}`
     }
 
     async upsertPassword(token: string, password: string): Promise<User> {
-        this.cleanUpExpiredTokens()
-        const email = Object.entries(this.temporaryToken).find(
-            ([, tokenForEmail]) => tokenForEmail === token,
-        )?.[1]
-        if (email === undefined) {
-            throw new UnauthorizedException('토큰 만료')
+        try {
+            const { email } =
+                await this.jwtService.verifyAsync<MagicLinkPayload>(token)
+            const hashedPassword = await bcrypt.hash(password, 10)
+            return this.prisma.user.upsert({
+                where: {
+                    email,
+                },
+                create: {
+                    email,
+                    hashedPassword,
+                    name: '테스트 이름', // TODO: 저장된 값에서 찾기
+                },
+                update: {
+                    hashedPassword,
+                },
+            })
+        } catch (e) {
+            throw new UnauthorizedException('유효하지 않은 토큰')
         }
-        const hashedPassword = await bcrypt.hash(password, 10)
-        return this.prisma.user.upsert({
-            where: {
-                email,
-            },
-            create: {
-                email,
-                hashedPassword,
-                name: '테스트 이름', // TODO: 저장된 값에서 찾기
-            },
-            update: {
-                hashedPassword,
-            },
-        })
     }
 
     async validateUser({
