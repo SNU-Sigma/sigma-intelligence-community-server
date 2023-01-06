d41541d5d1de75da26f15b1384ca78ab01d4bac7
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-07 00:17:31 +0900

    fix: 비밀번호 입력 및 로그인 안 되는 이슈 수정

--- src/auth/dto/login-credentials.dto.ts
+++ src/auth/dto/login-credentials.dto.ts
@@ -1,7 +1,9 @@
-import { IsEmail } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 export class LoginCredentialsDto {
     @IsEmail()
     email: string
+    @IsNotEmpty()
+    @IsString()
     password: string
 }
