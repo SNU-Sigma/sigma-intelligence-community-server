abdd815ce87c9f5033806f0aa735ef8c62479319
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-07 03:25:20 +0900

    feat: 비밀번호 템플릿 생성

--- src/templates/password.tsx
+++ src/templates/password.tsx
@@ -1,7 +1,28 @@
+import { Html } from '@react-email/html'
+import { Button } from '@react-email/button'
+
 interface Props {
     link: string
 }
 
 export default function Password({ link }: Props) {
-    return <div>Link: {link}</div>
+    return (
+        <Html lang='ko'>
+            <Button href={link} style={buttonStyle}>
+                비밀번호 설정
+            </Button>
+        </Html>
+    )
 }
+
+const buttonStyle = {
+    backgroundColor: '#7b0920',
+    borderRadius: '4px',
+    fontWeight: 'normal',
+    color: '#fff',
+    fontSize: '15px',
+    textDecoration: 'none',
+    textAlign: 'center',
+    display: 'block',
+    padding: '8px',
+} as const satisfies React.CSSProperties
