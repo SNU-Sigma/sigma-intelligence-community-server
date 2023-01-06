57a91e9c582f7dabda3375eae3b22cd0078e64ab
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-07 02:28:58 +0900

    feat: 시그마 부원들만 가입 가능하도록 제한하기 위한 임시 방편

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,5 +1,13 @@
+import { Member } from './models/Member'
+
 export const AuthConstants = {
     accessTokenExpiresIn: 604800000, // 7일 => ms
     cookieKey: 'jwt',
     magicLinkBaseUrl: 'http://localhost:5173', // TODO: 변경 필요
+    memberList: [{ email: 'jihoon416@snu.ac.kr', name: '서지훈' }],
+} as const satisfies {
+    accessTokenExpiresIn: number
+    cookieKey: string
+    magicLinkBaseUrl: string
+    memberList: ReadonlyArray<Member>
 }
