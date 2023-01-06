5c08ea471ca7130e13d463f3e7f82c9292efef5e
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-07 03:37:47 +0900

    refactor: Move auth guard to controller

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -4,12 +4,12 @@ import { PreSignedUrlDto } from './dto/pre-signed-url.dto'
 import { ApiTags } from '@nestjs/swagger'
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 
+@UseGuards(JwtAuthGuard)
 @Controller('images')
 @ApiTags('images')
 export class ImagesController {
     constructor(private readonly imagesService: ImagesService) {}
 
-    @UseGuards(JwtAuthGuard)
     @Get('pre-signed-url')
     async getPreSignedUrl(
         @Query('fileName') fileName: string,
