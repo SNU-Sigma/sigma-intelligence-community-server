ac2ca1eb3c81a2c2dfc911b01c6cb3021e37a89e
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-08 16:04:37 +0900

    chore: 테스트 위해 멤버 목록 추가

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -3,7 +3,11 @@ import { Member } from './models/Member'
 export const AuthConstants = {
     accessTokenExpiresIn: 604800000, // 7일 => ms
     cookieKey: 'jwt',
-    memberList: [{ email: 'jihoon416@snu.ac.kr', name: '서지훈' }],
+    memberList: [
+        { email: 'luorix@snu.ac.kr', name: '황진우' },
+        { email: 'jihoon416@snu.ac.kr', name: '서지훈' },
+        { email: 'tlsbamtol@snu.ac.kr', name: '신재우' },
+    ],
 } as const satisfies {
     accessTokenExpiresIn: number
     cookieKey: string
