a7622b01bfe488f493b3df43a7865975de8b754f
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-08 15:54:26 +0900

    improvement: ExtractUser 데코레이터 시범용 엔드포인트 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,23 @@
 import { Controller, Get } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { AppService } from './app.service'
+import { Public } from './utility/decorators/public.decorator'
+import { ExtractUser } from './utility/decorators/extract-user.decorator'
+import { User } from '@prisma/client'
 
 @Controller()
 @ApiTags('root')
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
+    @Public()
     @Get('health')
     getHello(): string {
         return this.appService.getHello()
+    }
+
+    @Get('__for_testing_extract_user')
+    getUserEmail(@ExtractUser() user: User): string {
+        return user.email
     }
 }
