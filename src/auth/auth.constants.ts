3ebf3b6539568b07e5d503468b8e1682a2fb1964
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-11 22:53:10 +0900

    chore: 백엔드 팀 명단 등록

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -7,6 +7,10 @@ export const AuthConstants = {
         { email: 'luorix@snu.ac.kr', name: '황진우' },
         { email: 'jihoon416@snu.ac.kr', name: '서지훈' },
         { email: 'tlsbamtol@snu.ac.kr', name: '신재우' },
+        { email: 'hjl1013@snu.ac.kr', name: '이현준' },
+        { email: 'naemduu@snu.ac.kr', name: '황두환' },
+        { email: 'lisi2337qar@snu.ac.kr', name: '박준하' },
+        { email: 'dongwooryu@snu.ac.kr', name: '류동우' },
     ],
 } as const satisfies {
     accessTokenExpiresIn: number
