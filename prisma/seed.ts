8694b603032910d16ae7b8ec7c53ab7164a7aa56
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-21 19:21:43 +0900

    improvement: 초기 유저 생성을 위한 seed script 추가

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,38 @@
 import { PrismaClient } from '@prisma/client'
+import * as bcrypt from 'bcrypt'
 
 const prisma = new PrismaClient()
 
-async function main() {}
+async function main() {
+    const userCount = 20
+    const hashedPassword = await bcrypt.hash('1111', 10)
+    await Promise.all(
+        Array.from({ length: userCount }, (_, index) => {
+            const email = `test${index.toString().padStart(4, '0')}@snu.ac.kr`
+            return prisma.user.upsert({
+                where: { email },
+                update: {},
+                create: {
+                    email,
+                    profile: {
+                        create: {
+                            name: Array.from({ length: 3 }, () =>
+                                String.fromCharCode(
+                                    44031 + Math.ceil(11172 * Math.random()),
+                                ),
+                            ).join(''),
+                        },
+                    },
+                    userAuth: {
+                        create: {
+                            hashedPassword,
+                        },
+                    },
+                },
+            })
+        }),
+    )
+}
 
 main()
     .then(async () => {
