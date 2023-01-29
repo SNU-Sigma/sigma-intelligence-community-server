d97d3c4dc8fc9638a28a686585c674150673de90
hjl1013 <hjl1013@snu.ac.kr>
2023-01-29 17:36:08 +0900

    디버깅 완료

--- src/printer-reservation/pipes/ParseDatePipe.pipe.ts
+++ src/printer-reservation/pipes/ParseDatePipe.pipe.ts
@@ -1,12 +1,8 @@
-import {
-    ArgumentMetadata,
-    BadRequestException,
-    PipeTransform,
-} from '@nestjs/common'
+import { BadRequestException, PipeTransform } from '@nestjs/common'
 import { isDate } from 'class-validator'
 
 export class ParseDatePipe implements PipeTransform {
-    transform(value: any, metadata: ArgumentMetadata) {
+    transform(value: string) {
         if (!isDate(value)) {
             throw new BadRequestException(`${value} isn't Date format`)
         }
