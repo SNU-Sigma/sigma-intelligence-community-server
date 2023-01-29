a0dfc99543e690d21b07d190fd52af1ddb9cd510
hjl1013 <hjl1013@snu.ac.kr>
2023-01-29 17:32:05 +0900

    디버깅 완료

--- src/printer-reservation/pipes/ParseDatePipe.pipe.ts
+++ src/printer-reservation/pipes/ParseDatePipe.pipe.ts
@@ -8,9 +8,7 @@ import { isDate } from 'class-validator'
 export class ParseDatePipe implements PipeTransform {
     transform(value: any, metadata: ArgumentMetadata) {
         if (!isDate(value)) {
-            throw new BadRequestException(
-                `${value} isn't int the status options`,
-            )
+            throw new BadRequestException(`${value} isn't Date format`)
         }
 
         return new Date(value)
