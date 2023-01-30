3fc7f7c3eb029f3114ea44c18292b46a776bccb0
hjl1013 <hjl1013@snu.ac.kr>
2023-01-31 04:22:29 +0900

    User 정보에 Role 추가

--- src/common/dto/user.dto.ts
+++ src/common/dto/user.dto.ts
@@ -1,9 +1,10 @@
-import { User } from '@prisma/client'
+import { Role, User } from '@prisma/client'
 import { UserProfileDto } from './user-profile.dto'
 
 export class UserDto implements User {
     id: number
     email: string
+    role: Role
     userAuthId: number
     profileId: number
 
