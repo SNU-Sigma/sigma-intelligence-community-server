028f16caea0585e7b05494eb2356b8612c7ba6ef
Jihoon Seo <jihoon416@snu.ac.kr>
2023-02-11 22:07:38 +0900

    feat: Winston - S3 stream으로 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common'
 import { ReactAdapter } from '@webtre/nestjs-mailer-react-adapter'
 import { WinstonModule } from 'nest-winston'
 import { PrismaModule } from 'nestjs-prisma'
+import { S3StreamLogger } from 's3-streamlogger'
 import { format, transports } from 'winston'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { AwsModule } from './aws/aws.module'
+import { AwsService } from './aws/aws.service'
 import { ConfigModule } from './config/config.module'
 import { ConfigService } from './config/config.service'
 import { ImagesModule } from './images/images.module'
@@ -19,13 +21,49 @@ import { PrinterReservationModule } from './printer-reservation/printer-reservat
     imports: [
         ConfigModule,
         PrismaModule.forRoot({ isGlobal: true }),
-        WinstonModule.forRoot({
-            transports: [
-                new transports.Console({
-                    format: format.combine(format.colorize(), format.simple()),
-                    level: 'verbose',
-                }),
-            ],
+        WinstonModule.forRootAsync({
+            imports: [AwsModule],
+            inject: [AwsService],
+            useFactory: (awsService: AwsService) => {
+                const s3Transports = (() => {
+                    if (process.env.NODE_ENV !== 'production') {
+                        return []
+                    }
+                    const {
+                        region,
+                        bucketName,
+                        folderName,
+                        secretAccessKey,
+                        accessKeyId,
+                    } = awsService.getSharedConfigurationForLogging()
+                    const transport = new transports.Stream({
+                        stream: new S3StreamLogger({
+                            region,
+                            bucket: bucketName,
+                            folder: folderName,
+                            access_key_id: accessKeyId,
+                            secret_access_key: secretAccessKey,
+                        }),
+                        level: 'error', // FIXME: 추후 더 다양한 로깅 넣기
+                    })
+                    transport.on('error', () => {
+                        // FIXME: S3 업로드 중에 발생하는 에러 처리하기
+                    })
+                    return [transport]
+                })()
+                return {
+                    transports: [
+                        new transports.Console({
+                            format: format.combine(
+                                format.colorize(),
+                                format.simple(),
+                            ),
+                            level: 'verbose',
+                        }),
+                        ...s3Transports,
+                    ],
+                }
+            },
         }),
         MailerModule.forRootAsync({
             imports: [ConfigModule],
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -9,8 +9,12 @@ declare module 's3-streamlogger' {
             folder: string
             access_key_id: string
             secret_access_key: string
+            /**
+             * Files will be rotated every `rotate_every` milliseconds. Defaults to 3600000 (60 minutes).
+             */
+            rotate_every?: number
         })
     }
 
-    export = S3StreamLogger
+    export { S3StreamLogger }
 }
