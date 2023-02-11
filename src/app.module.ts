1faa0079859356fdfe86390cb75bfa54f3e34151
Jihoon Seo <jihoon416@snu.ac.kr>
2023-02-12 00:46:54 +0900

    feat: Pino로 Winston 구현 대체

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { MailerModule } from '@nestjs-modules/mailer'
 import { Module } from '@nestjs/common'
 import { ReactAdapter } from '@webtre/nestjs-mailer-react-adapter'
-import {
-    utilities as nestWinstonModuleUtilities,
-    WinstonModule,
-} from 'nest-winston'
+import { LoggerModule } from 'nestjs-pino'
 import { PrismaModule } from 'nestjs-prisma'
 import { S3StreamLogger } from 's3-streamlogger'
-import { format, transports } from 'winston'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
@@ -16,7 +12,6 @@ import { AwsService } from './aws/aws.service'
 import { ConfigModule } from './config/config.module'
 import { ConfigService } from './config/config.service'
 import { ImagesModule } from './images/images.module'
-import { LoggerModule } from './logger/logger.module'
 import { PostsModule } from './posts/posts.module'
 import { PrinterReservationModule } from './printer-reservation/printer-reservation.module'
 
@@ -24,48 +19,33 @@ import { PrinterReservationModule } from './printer-reservation/printer-reservat
     imports: [
         ConfigModule,
         PrismaModule.forRoot({ isGlobal: true }),
-        WinstonModule.forRootAsync({
+        LoggerModule.forRootAsync({
             imports: [AwsModule],
             inject: [AwsService],
             useFactory: (awsService: AwsService) => {
-                const s3Transports = (() => {
-                    if (process.env.NODE_ENV !== 'production') {
-                        return []
-                    }
-                    const {
-                        region,
-                        bucketName,
-                        folderName,
-                        secretAccessKey,
-                        accessKeyId,
-                    } = awsService.getSharedConfigurationForLogging()
-                    const transport = new transports.Stream({
-                        stream: new S3StreamLogger({
-                            region,
-                            bucket: bucketName,
-                            folder: folderName,
-                            access_key_id: accessKeyId,
-                            secret_access_key: secretAccessKey,
-                        }),
-                        level: 'error', // FIXME: 추후 더 다양한 로깅 넣기
-                    })
-                    transport.on('error', () => {
-                        // FIXME: S3 업로드 중에 발생하는 에러 처리하기
-                    })
-                    return [transport]
-                })()
+                const {
+                    region,
+                    bucketName,
+                    folderName,
+                    secretAccessKey,
+                    accessKeyId,
+                } = awsService.getSharedConfigurationForLogging()
+                const stream = new S3StreamLogger({
+                    region,
+                    bucket: bucketName,
+                    folder: folderName,
+                    access_key_id: accessKeyId,
+                    secret_access_key: secretAccessKey,
+                })
+                stream.on('error', (error) => {
+                    console.error(error)
+                    // FIXME: S3 업로드 중에 발생하는 에러 처리하기
+                })
                 return {
-                    transports: [
-                        new transports.Console({
-                            format: format.combine(
-                                format.timestamp(),
-                                format.ms(),
-                                nestWinstonModuleUtilities.format.nestLike(),
-                            ),
-                            level: 'verbose',
-                        }),
-                        ...s3Transports,
-                    ],
+                    pinoHttp:
+                        process.env.NODE_ENV === 'production'
+                            ? stream
+                            : { transport: { target: 'pino-pretty' } },
                 }
             },
         }),
@@ -87,7 +67,6 @@ import { PrinterReservationModule } from './printer-reservation/printer-reservat
             },
             inject: [ConfigService],
         }),
-        LoggerModule,
         AwsModule,
         ImagesModule,
         AuthModule,
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { ValidationPipe } from '@nestjs/common'
 import { HttpAdapterHost, NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
+import { Logger } from 'nestjs-pino'
 import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma'
 import { AppModule } from './app.module'
 import { ConfigService } from './config/config.service'
 import { herokuSSLRedirect } from './utility/middleware/herokuSSLRedirect'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(AppModule, { bufferLogs: true })
 
-    app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
+    app.useLogger(app.get(Logger))
     app.use(herokuSSLRedirect())
 
     app.enableCors({
