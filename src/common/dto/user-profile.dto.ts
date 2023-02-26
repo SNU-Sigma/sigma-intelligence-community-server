c304be81a5c1177927c74d99961e08bf39be880e
Jihoon Seo <jihoon416@snu.ac.kr>
2023-02-26 12:48:47 +0900

    feat: UserProfile에 freshmanYear, major, profileImageUrl 필드 추가

--- src/common/dto/user-profile.dto.ts
+++ src/common/dto/user-profile.dto.ts
@@ -3,4 +3,7 @@ import { UserProfile } from '@prisma/client'
 export class UserProfileDto implements UserProfile {
     id: number
     name: string
+    freshmanYear: number | null
+    major: string | null
+    profileImageUrl: string | null
 }
