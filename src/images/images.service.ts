1bb5105b7828cf94e2619003dac6b1a14da8ca59
서지훈 <jihoon416@snu.ac.kr>
2023-02-26 14:37:17 +0900

    홈커밍 전시회 전 수정 (#37)
    * improvement: 이미지 S3에 저장 시 폴더 내부에 저장되도록 함
    
    * temp: 임시로 아무 이메일이나 가입 허용

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common'
-import { AwsService } from '../aws/aws.service'
 import * as uuid from 'uuid'
+import { AwsService } from '../aws/aws.service'
 
 @Injectable()
 export class ImagesService {
     constructor(private awsService: AwsService) {}
 
+    private readonly folderName = 'user-images/'
+
     async createPreSignedUrlFromFileName(fileName: string) {
-        const uniqueName = uuid.v4() + fileName
-        const url = await this.awsService.createPreSignedUrl(uniqueName)
-        const uploadedUrl = `${distributionBaseUrl}/${uniqueName}`
+        const key = this.folderName + uuid.v4() + '_' + fileName
+        const url = await this.awsService.createPreSignedUrl(key)
+        const uploadedUrl = `${distributionBaseUrl}/${key}`
         return {
             url,
             uploadedUrl,
